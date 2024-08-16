7257562bc9d60821e9579d4c2fa6b5f9ec3acac8
thutanyan <thutathuta319@gmail.com>
2024-08-16 20:10:18 +0630

    l5-Sidebar

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,18 +1,72 @@
 import React from 'react'
 import { cn } from '@/lib/utils'
+import Image from 'next/image'
+import Link from 'next/link'
+import SidebarItem from './sidebar-item'
+import {
+    ClerkLoading,
+    ClerkLoaded,
+    UserButton
+} from "@clerk/nextjs"
+import { Loader } from 'lucide-react'
 
-type Props={
-    className?:string
+type Props = {
+    className?: string
 }
 
-export default function Sidebar({className}:Props) {
+export default function Sidebar({ className }: Props) {
     return (
         <div className={
-            cn('flex bg-blue-600 h-full lg:w-[256px] lg:fixed left-0 top-0 px-4 border-r-2 flex-col',
+            cn('flex  h-full lg:w-[256px] lg:fixed left-0 top-0 px-4 border-r-2 flex-col',
                 className
             )
         }>
-            Sidebar
+            <Link href="/learn">
+                <div className='pt-8 pl-4 pb-7 flex items-center gap-x-3'>
+                    <Image src="/mascot.svg" height={40} width={40} alt="Mascot" />
+                    <h1 className='text-2xl font-extrabold text-green-600 tracking-wide'>
+                        Lingo
+                    </h1>
+                </div>
+            </Link>
+
+            <div className=' flex flex-col gap-y-2 flex-1'>
+                <SidebarItem
+                    label="learn"
+                    href="/learn"
+                    iconSrc='/learn.svg'
+                />
+
+                <SidebarItem
+                    label="leaderboard"
+                    href="/leaderboard"
+                    iconSrc='/leaderboard.svg'
+                />
+
+
+                <SidebarItem
+                    label="quests"
+                    href="/quests"
+                    iconSrc='/quests.svg'
+                />
+
+
+
+                <SidebarItem
+                    label="shop"
+                    href="/shop"
+                    iconSrc='/shop.svg'
+                />
+            </div>
+
+            <div className="p-4 ">
+                <ClerkLoading>
+                    <Loader className="h-5 w-5 text-muted-foreground animate-spin" />
+                </ClerkLoading>
+                <ClerkLoaded>
+                    <UserButton/>
+                </ClerkLoaded>
+            </div>
         </div>
     )
 }
