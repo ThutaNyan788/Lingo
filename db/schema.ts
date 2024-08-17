b09b3babfaf02381057273be6a1e67af97652666
thutanyan <thutathuta319@gmail.com>
2024-08-17 11:57:16 +0630

    l7-Drizzle-&-Neon-Setup

---
+++ db/schema.ts
@@ -0,0 +1,7 @@
+import { pgTable,serial,uuid,text } from "drizzle-orm/pg-core";
+
+export const courses = pgTable("courses",{
+    id:serial("id").primaryKey(),
+    title:text("title").notNull(),
+    imageSrc:text("image_src").notNull(),
+})
